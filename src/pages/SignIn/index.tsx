2f3349f276b6db5b2e21b98f60369df31fd94d1f
Lucas Santos <lucas201273@gmail.com>
2021-11-21 12:18:14 -0300

    chore: connect redux with signin page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { StoreState } from '../../store/createStore'
+import * as actions from '../../store/modules/auth/actions'
 import image from '../../assets/img/redux.jpg'
 import './styles.css'
 
 export function SignIn () {
+  const dispatch = useDispatch()
+  const { loadignSignIn } = useSelector((state: StoreState) => state.auth)
+  console.log('LOADING SIGNED IN:', loadignSignIn)
+
   return (
     <div className="form-container">
     <img src={image} alt="redux logo"/>
-    <form>
-      <input type="text" placeholder="Email"/>
-      <input type="password" placeholder="Senha"/>
-      <button> Entrar</button>
-    </form>
+    <div className="form">
+      <input type="text" placeholder="Email" defaultValue="lucas@1273@gmail.com"/>
+      <input type="password" placeholder="Senha" defaultValue="213"/>
+      <button onClick={() => dispatch(actions.signRequest({ email: 'lucas@1273@gmail.com', password: '213' }))}> {loadignSignIn ? 'Carregando' : 'Entrar'}</button>
+    </div>
   </div>
   )
 }
