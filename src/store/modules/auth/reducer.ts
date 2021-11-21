8a665ced65e88e9ec916c1fb3c0e69d7bbe88a54
Lucas Santos <lucas201273@gmail.com>
2021-11-21 11:30:56 -0300

    chore: add type actions on actions

--- src/store/modules/auth/reducer.ts
+++ src/store/modules/auth/reducer.ts
@@ -1,17 +1,16 @@
-import { AnyAction } from 'redux'
-import { AuthState } from './types'
+
+import { AuthState, AuthActions } from './types'
 
 const initialState: AuthState = {
   loadignSignIn: false,
   isSignedIn: false
 }
 
-function reducer (state = initialState, action: AnyAction): AuthState {
+function reducer (state = initialState, action: AuthActions): AuthState {
   switch (action.type) {
     case '@auth/SIGN_IN_REQUEST': {
       const newState = { ...state }
       newState.loadignSignIn = true
-
       return newState
     }
 
--- src/store/modules/auth/types.ts
+++ src/store/modules/auth/types.ts
@@ -1,3 +1,8 @@
+import { ActionType } from 'typesafe-actions'
+import * as actions from './actions'
+
+export type AuthActions = ActionType<typeof actions>
+
 export interface AuthState {
   readonly loadignSignIn: boolean
   readonly isSignedIn: boolean
