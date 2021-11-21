0417cf899390745fee2219d66bdac05ba6e553d4
Lucas Santos <lucas201273@gmail.com>
2021-11-21 11:34:03 -0300

    chore: move return of action to typesafeaction

--- src/store/modules/auth/actions.ts
+++ src/store/modules/auth/actions.ts
@@ -1,12 +1,14 @@
-
+import { action } from 'typesafe-actions'
 interface SignInRequestData{
   email: string
   password: string
 }
 
 export function signRequest ({ email, password }: SignInRequestData) {
-  return {
-    type: '@auth/SIGN_IN_REQUEST',
-    payload: { email, password }
-  }
+  return action('@auth/SIGN_IN_REQUEST', { email, password })
 }
+
+// {
+//  type: '@auth/SIGN_IN_REQUEST',
+//  payload: { email, password }
+// }
